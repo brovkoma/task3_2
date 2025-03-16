e5d61f910b0ca4f56778ad9c544937af2cdcfcf7
brovkoma <brovkoma18@rambler.ru>
2025-03-16 17:55:14 +0400

    sequence is starting from 0

--- task3.cpp
+++ task3.cpp
@@ -3,12 +3,13 @@
 using namespace std;
 
 int f(int n) {
-    if (n == 1 || n == 2) {
+    if (n == 1) {
+        return 0;
+    }
+    if (n == 2) {
         return 1;
     }
-    else {
-        return f(n - 1) + f(n - 2);
-    }
+    return f(n - 1) + f(n - 2);
 }
 
 int main()
