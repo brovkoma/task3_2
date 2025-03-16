ecd22c344bc13f2d81d30e8dcc96bcffad6329da
brovkoma <brovkoma18@rambler.ru>
2025-03-16 17:56:02 +0400

    iterative fibonacci

--- task3.cpp
+++ task3.cpp
@@ -2,21 +2,23 @@
 
 using namespace std;
 
-int f(int n) {
-    if (n == 1) {
-        return 0;
-    }
-    if (n == 2) {
-        return 1;
-    }
-    return f(n - 1) + f(n - 2);
-}
-
 int main()
 {
     setlocale(LC_ALL, "rus");
     cout << "Введите n" << endl;
     int n;
     cin >> n;
-    cout << f(n) << endl;
+
+    int* a = new int[n];
+
+    a[0] = 0;
+    a[1] = 1;
+
+    for (int i = 2; i < n; i++) {
+        a[i] = a[i - 1] + a[i - 2];
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << endl;
+    }
 }
